4ecc0444c7e589e559395f2eb2537c5be0e6222a
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-16 17:41:54 +0900

    Add server.go

---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"my-trophy-prototype-api/config"
+	"my-trophy-prototype-api/handler"
+
+	"my-trophy-prototype-api/middleware"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// load config
+	c := config.NewConfig()
+
+	// create Echo
+	e := echo.New()
+
+	// Set middlewares
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS(c.AllowOrigins))
+
+	// Create database connection
+	db, err := gorm.Open("mysql", c.DBUserName+":"+c.DBPassword+"@tcp("+c.DBHost+":"+c.DBPort+")/mytrophy?charset=utf8mb4&collation=utf8mb4_bin&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Initialize handler
+	h := &handler.Handler{DB: db}
+
+	// Bind routes
+	e.GET("/users/:id", h.FindUserByID)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":" + c.PORT))
+}
