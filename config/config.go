1366cd3e13fc1e2cd049bbe4bf6b0b38163688a2
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-16 17:37:22 +0900

    Add config.go

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+)
+
+// Config is config
+type Config struct {
+	DBHost       string
+	DBPort       string
+	DBUserName   string
+	DBPassword   string
+	PORT         string
+	AllowOrigins string
+}
+
+// NewConfig is constructor for Config
+func NewConfig() Config {
+	conf := Config{
+		DBHost:       os.Getenv("DB_HOST"),
+		DBPort:       os.Getenv("DB_PORT"),
+		DBUserName:   os.Getenv("DB_USER_NAME"),
+		DBPassword:   os.Getenv("DB_PASSWORD"),
+		PORT:         os.Getenv("PORT"),
+		AllowOrigins: os.Getenv("ALLOW_ORIGINS"),
+	}
+	return conf
+}
